eb81440e37f7d528360ab423173f4c7e0f12695d
심현진 <hyunjin.dev143@gmail.com>
2024-12-20 00:39:41 +0900

    chore : 코드 들여쓰기 수정

--- flask-server/db.py
+++ flask-server/db.py
@@ -1,10 +1,10 @@
 import pymysql
 
 def get_db_connection():
-    return pymysql.connect(
-        host="localhost",
-        user="flask_user",
-        password="q1w2e3",
-        database="gcode_db",
-        cursorclass=pymysql.cursors.DictCursor
-    )
+  return pymysql.connect(
+    host="localhost",
+    user="flask_user",
+    password="q1w2e3",
+    database="gcode_db",
+    cursorclass=pymysql.cursors.DictCursor
+	)
